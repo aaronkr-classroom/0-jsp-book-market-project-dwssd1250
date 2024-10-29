43646fce903efd79e322848d9652115e97887acd
dwssd1250 <uuer248@gmail.com>
2024-10-29 17:25:44 +0900

    w4

--- src/main/java/dto/Book.java
+++ src/main/java/dto/Book.java
@@ -27,63 +27,85 @@ public class Book implements Serializable{
 		this.unitPrice = unitPrice;
 	}
 	
+	public int getUnitPrice() {
+		return unitPrice;
+	}
+
+	public void setUnitPrice(int unitPrice) {
+		this.unitPrice = unitPrice;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getPublisher() {
+		return publisher;
+	}
+
+	public void setPublisher(String publisher) {
+		this.publisher = publisher;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+	public long getUnitsInStock() {
+		return unitsInStock;
+	}
+
+	public void setUnitsInStock(long unitsInStock) {
+		this.unitsInStock = unitsInStock;
+	}
+
+	public String getReleaseDate() {
+		return releaseDate;
+	}
+
+	public void setReleaseDate(String releaseDate) {
+		this.releaseDate = releaseDate;
+	}
+
+	public String getCondition() {
+		return condition;
+	}
+
+	public void setCondition(String condition) {
+		this.condition = condition;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
 	public String getBookId() { return bookId; }
 	
 	public void setBookId(String bookId) {
 		this.bookId=bookId;
 	}
 	
-	public String getname() { return name; }
+	public String getName() { return name; }
 	
-	public void setname(String name) {
-		this.name=name;
-	}
 	
-	public int getunitPrice() { return unitPrice; }
-	
-	public void setunitPrice(int unitPrice) {
-		this.unitPrice=unitPrice;
-	}
-	
-	public String getauthor() { return author; }
-	
-	public void setauthor(String author) {
-		this.author=author;
-	}
-	
-	public String getdescription() { return description; }
-	
-	public void setdescription(String description) {
-		this.description=description;
-	}
-	
-	public String getpublisher() { return publisher; }
-	
-	public void setpublisher(String publisher) {
-		this.publisher=publisher;
-	}
-	
-	public String getcategory() { return category; }
-	
-	public void setcategory(String category) {
-		this.category=category;
-	}
-	
-	public long getunitsInStock() { return unitsInStock; }
-	
-	public void setunitsInStock(long unitsInStock) {
-		this.unitsInStock=unitsInStock;
-	}
-	
-	public String getreleaseDate() { return releaseDate; }
-	
-	public void setreleaseDate(String releaseDate) {
-		this.releaseDate=releaseDate;
-	}
-	
-	public String getcondition() { return condition; }
-	
-	public void setcondition(String condition) {
-		this.condition=condition;
-	}
 }
